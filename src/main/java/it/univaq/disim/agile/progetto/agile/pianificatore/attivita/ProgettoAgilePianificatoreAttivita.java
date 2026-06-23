d301171739c4512e8108d9f5250d9c99fca10f11
Filippo <Filippo@DESKTOP-PQERFCF>
2026-06-23 16:08:43 +0200

    Sprint 0: Setup infrastruttura Maven(con jdk24)

---
+++ src/main/java/it/univaq/disim/agile/progetto/agile/pianificatore/attivita/ProgettoAgilePianificatoreAttivita.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.univaq.disim.agile.progetto.agile.pianificatore.attivita;
+
+/**
+ *
+ * @author Filippo
+ */
+public class ProgettoAgilePianificatoreAttivita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

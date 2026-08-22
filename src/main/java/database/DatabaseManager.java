183ce3c99b54cef10ee52167e0e185563c7c5ad9
Edoardo Marrancone <edoardo.marrancone@gmail.com>
2026-08-22 22:35:09 +0200

    connessione database

---
+++ src/main/java/database/DatabaseManager.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author edoar
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    // Cambia questi valori con quelli del tuo database!
+    private static final String URL = "jdbc:mysql://localhost:3306/progetto_agile_pianificatore_attivita?noAccessToProcedureBodies=true&serverTimezone=Europe/Rome";
+    private static final String USER = "root"; // Il tuo username MySQL
+    private static final String PASSWORD = "12345"; // La tua password MySQL
+
+    /**
+     * Metodo statico per ottenere la connessione al DBMS. Come da appunti, si
+     * utilizza DriverManager.getConnection.
+     */
+    public static Connection getConnection() throws SQLException {
+        // DriverManager restituisce un oggetto Connection pronto all'uso
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
+

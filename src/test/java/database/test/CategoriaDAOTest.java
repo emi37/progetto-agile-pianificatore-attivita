25fcddd7f2cd67e4afb565dcf5c9c47e919ada8c
Filippo <Filippo@DESKTOP-PQERFCF>
2026-09-15 21:41:57 +0200

    daotest aggiustato

--- src/test/java/database/test/CategoriaDAOTest.java
+++ src/test/java/database/test/CategoriaDAOTest.java
@@ -5,23 +5,25 @@ import it.univaq.disim.agile.progetto.agile.pianificatore.attivita.domain.Catego
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**classe di testing per verificare l'inserimento delle nuove categorie
- */
+/**classe di testing per verificare l'inserimento delle categorie
+ */
 public class CategoriaDAOTest {
-
-    @Test
-    public void testInserimentoNuovaCategoria() {
-        
-        CategoriaDAO dao = new CategoriaDAO();
-        
-        // 1.scelgo un nome per la categoria finta e un id utente (es. 2)
-        String nomeCategoria = "Categoria di Prova";
-        int idUtente = 2;
-        
-        // 2. simulo l'inserimento nel db
-        Categoria risultato = dao.inserisciCategoriaCustom(nomeCategoria, idUtente);
-        
-        // 3. faccio l'asserzione, verifico che succede quello che mi aspetto,cioè che il db abbia accettato l'inserimento restituendomi un oggetto di tipo "categoria" valido(quindi non nullo)
-        assertNotNull(risultato, "l'inserimento della nuova categ. ha dato errore");
-    }
-}   
+    @Test
+    public void testInserimentoNuovaCategoria() {
+        
+        CategoriaDAO dao = new CategoriaDAO();
+        
+        
+        
+        // 1. scelgo un nome per la categoria finta e un id utente (es. 2)
+        String nomeCategoria = "Categoria di Prova";
+        int idUtente = 2;
+        
+        // 2. simulo l'inserimento nel database
+        Categoria risultato = dao.inserisciCategoriaCustom(nomeCategoria, idUtente);
+        
+        // 3. faccio l'asserzione: verifico che il db abbia accettato l'inserimento 
+        // restituendomi un oggetto Categoria valido (non nullo)
+        assertNotNull(risultato, "l'inserimento della categoria ha dato errore");
+    }
+}
